e27f3c4685e1e5fa4371a6ed48e6d159b9d2d539
Saul Campos Berardo <camposb@tcu.gov.br>
2018-05-27 16:06:26 -0300

    Download adn extracting imdb file from original source

--- imdb/imdb.py
+++ imdb/imdb.py
@@ -7,30 +7,56 @@ import math
 import os
 from tqdm import tqdm
 import requests
+import tarfile
 
 
-def download_file(url, destination_folder='.', chunk_size=1024):
+def download_file(url, out_file, chunk_size=1024):
     """
     Download file from url and save in the destinatin path.
     """
     # Get file headers
     response = requests.get(url, stream=True)
     
-    # Get file length and the number of chunks that will be downlaoded
+    # Get file length and determine the number of chunks that will be downloaded
     file_length = int(response.headers.get('content-length'))
     number_of_chunks = math.ceil(file_length//chunk_size) 
     
-    # Get the file name from the URL
-    file_name = url.split('/')[-1]
-    
     # Open file
-    with open(os.path.join(destination_folder, file_name), "wb") as f:
+    with open(out_file, "wb") as f:
         
         #  Write each chunk at a time        
         for data in tqdm(response.iter_content(chunk_size), total=number_of_chunks, unit='KB', unit_scale=True):    
             f.write(data)
 
+    
+def un_gzip_tar_file(in_file, destination_folder=None):
+    """ Extract content from in_file and save it in destination_folder (or . if None is passed)"""
+    destination_folder = os.path.dirname(in_file) if destination_folder is None else destination_folder    
+    tfile = tarfile.open(in_file, 'r:gz')
+    tfile.extractall(destination_folder)
 
 if __name__=='__main__':
-    url = "http://ai.stanford.edu/~amaas/data/sentiment/aclImdb_v1.tar.gz"
-    download_file(url, '..')
+ 
+    DESTINATION_FOLDER = '..'
+    DESTINATION_FILE = 'aclImdb_v1.tar.gz'
+    IMDB_URL = "http://ai.stanford.edu/~amaas/data/sentiment/aclImdb_v1.tar.gz"
+    
+    
+    # Download file if it doesn't already exists
+    download_output = os.path.join(DESTINATION_FOLDER, DESTINATION_FILE)    
+    if not os.path.exists(download_output):        
+        print(f'Downloading file to: {download_output}')
+        download_file(IMDB_URL, download_output)
+    
+    # Extract files from tar.gz
+    aclImdb_folder = os.path.join(DESTINATION_FOLDER, 'aclImdb')
+    if not os.path.exists(aclImdb_folder):
+        print(f'Extracting files to: {aclImdb_folder}')
+        un_gzip_tar_file(download_output, DESTINATION_FOLDER)
+    
+    
+    
+    
+    
+    
+    
